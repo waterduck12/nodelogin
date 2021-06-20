a2aadcf38c2a214a5512bc2dbd434aadfd8d4a4b
waterduck12 <hyeon6423@naver.com>
2021-06-21 01:22:17 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -4,11 +4,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+
+
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 // 라우팅
 const home = require("./src/routes/home");
+
 
 //앱 세팅
 app.set("views", "./src/views");
@@ -16,6 +22,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true }));
+app.use(morgan('dev'));
+app.use(morgan('common', { stream : accessLogStream }));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
