272561fe0058ccafae87268c012da9b9bbabe5ad
waterduck12 <hyeon6423@naver.com>
2021-06-22 22:06:21 +0900

    백엔드 맛보기 최종 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,7 +6,7 @@ class UserStorage {
 
     static getUserInfo(id) {
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM abc WHERE id = ?;';
+            const query = 'SELECT * FROM users WHERE id = ?;';
             db.query(query, [id], (err, data) =>{
                 if (err) reject(`${err}`);
                 else resolve(data[0]);
@@ -16,7 +16,7 @@ class UserStorage {
 
     static async save(userInfo) {
         return new Promise((resolve, reject) => {
-            const query = "INSERT INTO abc(id, name, psword) VALUES(?, ?, ?);";
+            const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
             db.query(query, 
                 [userInfo.id, userInfo.name, userInfo.psword], 
                 (err) =>{
